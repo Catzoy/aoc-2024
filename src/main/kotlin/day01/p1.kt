b4b5994d97dcf4644d539fe9596c3fa9a9252735
Catzoy <demchenko20021@gmail.com>
2024-12-01 09:25:14 +0200

    Day 1 - part 1

--- src/main/kotlin/day01/p1.kt
+++ src/main/kotlin/day01/p1.kt
@@ -1,5 +1,31 @@
 package day01
 
-fun main() {
+import java.io.File
+import kotlin.math.absoluteValue
 
+fun main() {
+    val lines = File("src/main/kotlin/day01/input.txt")
+        .readLines()
+
+    val result = lines.asSequence()
+        .flatMap { line ->
+            line.split("   ")
+                .map { it.toInt() }
+                .withIndex()
+        }
+        .groupBy(
+            keySelector = { it.index },
+            valueTransform = { it.value }
+        )
+        .mapValues { (_, list) ->
+            list.sorted()
+        }
+        .let { grouping ->
+            grouping.getValue(0).zip(grouping.getValue(1))
+        }
+        .sumOf { (right, left) ->
+            (right - left).absoluteValue
+        }
+
+    print(result)
 }
