460bb8d88c51c3f3bd4f90f06272a3b919a51437
Catzoy <demchenko20021@gmail.com>
2024-12-01 09:31:56 +0200

    Day 1 - part 2

--- src/main/kotlin/day01/p2.kt
+++ src/main/kotlin/day01/p2.kt
@@ -1,5 +1,35 @@
 package day01
 
-fun main() {
+import java.io.File
 
+fun main() {
+    val lines = File("src/main/kotlin/day01/input.txt")
+        .readLines()
+
+    val (lefts, rights) = lines.asSequence()
+        .flatMap { line ->
+            line.split("   ")
+                .map { it.toInt() }
+                .withIndex()
+        }
+        .groupBy(
+            keySelector = { it.index },
+            valueTransform = { it.value }
+        )
+        .mapValues { (_, list) ->
+            list.sorted()
+        }
+        .let { grouping ->
+            grouping.getValue(0) to grouping.getValue(1)
+        }
+
+    val occurrences = buildMap<Int, Int> {
+        rights.forEach {
+            put(it, getOrDefault(it, 0) + 1)
+        }
+    }
+    val result = lefts.sumOf {
+        it * occurrences.getOrDefault(it, 0)
+    }
+    print(result)
 }
