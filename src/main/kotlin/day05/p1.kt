50940781bfed4e0bf8df15e96c81c87b5febfcbf
Catzoy <demchenko20021@gmail.com>
2024-12-05 11:20:55 +0200

    Day 5 - prettify

--- src/main/kotlin/day05/p1.kt
+++ src/main/kotlin/day05/p1.kt
@@ -2,37 +2,41 @@ package day05
 
 import utils.readInput
 
-fun main() {
-    val input = readInput("05").iterator()
-    var didReadAllRules = false
-    val rules = buildMap<Int, Set<Int>> {
-        while (!didReadAllRules) {
-            val line = input.next()
-            if (line.isEmpty()) {
-                didReadAllRules = true
-                continue
-            }
+fun Iterator<String>.readRules(): Map<Int, Set<Int>> {
+    return buildMap {
+        var line = next()
+        do {
             val (x, y) = line.split("|").map { it.toInt() }
             put(x, getOrDefault(x, emptySet()) + y)
+            line = next()
+        } while (line.isNotEmpty())
+    }
+}
+
+fun Iterator<String>.readUpdates(): List<List<Int>> {
+    return buildList {
+        while (hasNext()) {
+            add(next().split(",").map { it.toInt() })
         }
     }
-    val updates = buildList {
-        while (input.hasNext()) {
-            add(input.next().split(",").map { it.toInt() })
+}
+
+fun didBreakRules(rules: Map<Int, Set<Int>>, pages: List<Int>, j: Int): Boolean {
+    val page = pages[j]
+    val rule = rules.getOrDefault(page, emptySet())
+    return pages.take(j).any { it in rule }
+}
+
+fun main() {
+    val input = readInput("05").iterator()
+    val (rules, updates) = input.readRules() to input.readUpdates()
+    val result = updates.asSequence()
+        .filter { pages ->
+            pages.indices.none { j ->
+                didBreakRules(rules, pages, j)
+            }
+        }.sumOf {
+            it[it.size / 2]
         }
-    }
-    var accumulator = 0
-    for (pages in updates) {
-        var didFail = false
-        for ((j, page) in pages.withIndex()) {
-            val rule = rules[page] ?: continue
-            val preceding = pages.subList(0, j)
-            didFail = preceding.any { it in rule }
-            if (didFail) break
-        }
-        if (!didFail) {
-            accumulator += pages[pages.size / 2]
-        }
-    }
-    print(accumulator)
+    print(result)
 }
--- src/main/kotlin/day05/p2.kt
+++ src/main/kotlin/day05/p2.kt
@@ -2,56 +2,31 @@ package day05
 
 import utils.readInput
 
-fun Map<Int, Set<Int>>.didBreakRules(pages: List<Int>, j: Int): Boolean {
-    val page = pages[j]
-    val rule = getOrDefault(page, emptySet())
-    return pages.take(j).any { it in rule }
+fun List<Int>.indexOfRuleBreak(rules: Map<Int, Set<Int>>): Int? {
+    return indices.firstOrNull { j -> didBreakRules(rules, this, j) }
 }
 
 fun main() {
     val input = readInput("05").iterator()
-    var didReadAllRules = false
-    val rules = buildMap<Int, Set<Int>> {
-        while (!didReadAllRules) {
-            val line = input.next()
-            if (line.isEmpty()) {
-                didReadAllRules = true
-                continue
-            }
-            val (x, y) = line.split("|").map { it.toInt() }
-            put(x, getOrDefault(x, emptySet()) + y)
-        }
-    }
-    val updates = buildList {
-        while (input.hasNext()) {
-            add(input.next().split(",").map { it.toInt() })
-        }
-    }
-    val failed = buildList {
-        for (pages in updates) {
-            var didFail = false
-            for (j in pages.indices) {
-                didFail = rules.didBreakRules(pages, j)
-                if (didFail) break
-            }
-            if (didFail) {
-                add(pages)
+    val (rules, updates) = input.readRules() to input.readUpdates()
+    val result = updates.asSequence()
+        .filter { pages ->
+            pages.indices.any { j ->
+                didBreakRules(rules, pages, j)
             }
         }
-    }
-    val fixed = buildList {
-        for (pages in failed) {
-            val p = pages.toMutableList()
-            do {
-                val i = p.indices.first { j ->
-                    rules.didBreakRules(p, j)
+        .map { pages ->
+            pages.toMutableList().apply {
+                var i = indexOfRuleBreak(rules)
+                while (i != null) {
+                    val value = removeAt(i)
+                    add(i - 1, value)
+                    i = indexOfRuleBreak(rules)
                 }
-                val value = p.removeAt(i)
-                p.add(i - 1, value)
-            } while (p.indices.any { j -> rules.didBreakRules(p, j) })
-            add(p)
+            }
         }
-    }
-    val result = fixed.sumOf { it[it.size / 2] }
+        .sumOf {
+            it[it.size / 2]
+        }
     print(result)
 }
